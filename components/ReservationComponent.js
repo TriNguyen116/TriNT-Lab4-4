751b0da7889ffed6c918c3e2653c0642588e2c41
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2023-09-25 19:09:24 +0700

    Update-reservation-component-book-talbe

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, ScrollView, Text, Switch, Button } from 'react-native';
+import { StyleSheet, View, ScrollView, Text, Switch, Button, Modal } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { format } from 'date-fns';
+
+class ModalContent extends Component {
+  render() {
+    return (
+      <View style={styles.modal}>
+        <Text style={styles.modalTitle}>Your Reservation</Text>
+        <Text style={styles.modalText}>Number of Guests: {this.props.guests}</Text>
+        <Text style={styles.modalText}>Smoking?: {this.props.smoking ? 'Yes' : 'No'}</Text>
+        <Text style={styles.modalText}>Date and Time: {format(this.props.date, 'dd/MM/yyyy - HH:mm')}</Text>
+        <Button title='Close' color='#7cc' onPress={() => this.props.onPressClose()} />
+      </View>
+    );
+  }
+}
 
 class Reservation extends Component {
   constructor(props) {
@@ -12,7 +26,8 @@ class Reservation extends Component {
       guests: 1,
       smoking: false,
       date: new Date(),
-      showDatePicker: false
+      showDatePicker: false,
+      showModal: false
     }
   }
   render() {
@@ -44,11 +59,18 @@ class Reservation extends Component {
         <View style={styles.formRow}>
           <Button title='Reserve' color='#7cc' onPress={() => this.handleReservation()} />
         </View>
+
+        <Modal animationType={'slide'} visible={this.state.showModal}
+          onRequestClose={() => this.setState({ showModal: false })}>
+          <ModalContent guests={this.state.guests} smoking={this.state.smoking} date={this.state.date}
+            onPressClose={() => this.setState({ showModal: false })} />
+        </Modal>
       </ScrollView>
     );
   }
   handleReservation() {
-    alert(JSON.stringify(this.state));
+    //alert(JSON.stringify(this.state));
+    this.setState({ showModal: true });
   }
 }
 export default Reservation;
@@ -56,5 +78,8 @@ export default Reservation;
 const styles = StyleSheet.create({
   formRow: { alignItems: 'center', justifyContent: 'center', flex: 1, flexDirection: 'row', margin: 20 },
   formLabel: { fontSize: 18, flex: 2 },
-  formItem: { flex: 1 }
+  formItem: { flex: 1 },
+  modal: { justifyContent: 'center', margin: 20 },
+  modalTitle: { fontSize: 24, fontWeight: 'bold', backgroundColor: '#7cc', textAlign: 'center', color: 'white', marginBottom: 20 },
+  modalText: { fontSize: 18, margin: 10 }
 });
